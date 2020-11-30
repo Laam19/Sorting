e9ef93891c8c79559f835677403a15adffe4dbe2
Laam19 <akib19@student.sust.edu>
2020-11-30 18:53:00 +0600

    refactored

--- Sort/Sort/Program.cs
+++ Sort/Sort/Program.cs
@@ -7,12 +7,7 @@ namespace Sort
     {
         static void Main(string[] args)
         {
-            //// int[] UnsortedArray = new[] { 1, 5, 6, 2, 4, 3 };
-            //List<int> UnSortedArray = new List<int>() { 1, 2, 5, 7, 8, 10 };
-            //Console.WriteLine(UnSortedArray[0]);
-            //Console.WriteLine(UnSortedArray);
-            //List<int> SortedArray = new List<int>();
-            //SortedArray = SortDotNet<int>.Sort(UnSortedArray, SortType.Bubble, SortOrder.Decending);
+            
             int[] UnsortedArray = new[] { 1, 5, 6, 2, 4, 3 };
             int [] SortedArray1=SortDotNet<int>.Sort(UnsortedArray, SortType.Bubble, SortOrder.Decending);
             Console.WriteLine("Normal Sorting::");
